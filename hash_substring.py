0c12a3b670982487e79237fac1a8fd63c1d3d58f
Kristaps773 <84283476+Kristaps773@users.noreply.github.com>
2023-04-15 14:37:16 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -4,7 +4,6 @@ import compute_hash
 
 
 def read_input(filename):
-    
     input_type = input().rstrip()
 
     if input_type == 'I':
@@ -24,44 +23,23 @@ def read_input(filename):
         raise ValueError(f"Invalid input type: {input}")
     
     return pattern, text
-            
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and
-    # capital f (input from file) to choose which input 
-    # type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    #return (input().rstrip(), input().rstrip())
+   
 
 def print_occurrences(output):
-    # this function should control output, it doesn't 
-    # need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using 
-    # Rabin Karp alghoritm
     prime = 10**9 + 7
     base = 256
-    
-    patterhash = 0
-    texthash = 0
+
     pattern_len, text_len = len(pattern), len(text)
     pattern_hash = compute_hash(pattern, prime, base)
     text_hash = compute_hash(text[:pattern_len], prime, base)
     occur = set()
     if pattern_hash == text_hash and pattern == text[:pattern_len]:
-        occur
+        occur.add(0)
     for i in range(1, text_len - pattern_len + 1):
-        text_hash = (text_hash * base + ord(text[i -1])) * pow(base, pattern_len, prime) + ord(text[i + pattern_len -1]) % prime
+        text_hash = ((text_hash * base) % prime + ord(text[i + pattern_len -1]) - (ord(text[i -1]) * pow(base, pattern_len, prime)) % prime) % prime
         if pattern_hash == text_hash and pattern == text[i:i + pattern_len]:
             occur.add(i)
     return occur
